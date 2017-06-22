dbf4427f15a692cc217e96ec9608e99feabaef62
Michael <wolflearned91@gmail.com>
2017-06-22 01:46:29 -0400

    prep for register page

--- client/src/components/pages/home-page.js
+++ client/src/components/pages/home-page.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-
+import Login from '../auth/login'
 class HomePage extends Component {
   render() {
     return (
-      <div>Hello world! This is the home page route.</div>
+      <div className="title">
+      <h2>read less. say less.</h2>
+      <h1>seemē.</h1>
+      <Login />
+      </div>
     );
   }
 }
