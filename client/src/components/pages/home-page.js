b60aae15de36470a7fc8cfba39af57614ef524dc
Michael <wolflearned91@gmail.com>
2017-06-23 00:45:20 -0400

    edit profile functional

--- client/src/components/pages/home-page.js
+++ client/src/components/pages/home-page.js
@@ -5,7 +5,7 @@ class HomePage extends Component {
     return (
       <div>
         <div className="title">
-          <h2>We help you seemē. The REAL mē.</h2>
+          <h2>seemē for real.</h2>
           <h4>Swipe on 10s profile videos - understand a little better who you're "liking". The days of reading profiles are gone - Your profile comes to life by showing the real him or her, the real you. Meet new people nearby. See more of what you're signing up for. Simply, seemē.</h4>
         </div>
         <Register />
