44c44c178d62e93857282f3f70fb20696f96937a
Michael <wolflearned91@gmail.com>
2017-06-23 00:39:30 -0400

    edit profile working

--- client/src/components/dashboard/profile/edit-info.js
+++ client/src/components/dashboard/profile/edit-info.js
@@ -1,12 +1,13 @@
 const axios = require('axios');
 const React = require('react');
-
-var editInfo = React.createClass({
+const cookie = require('react-cookie');
+const editInfo = React.createClass({
 
   getInitialState: function () {
     return {
       firstName: "",
-      email: "",
+      is_male: "",
+      seeking_male: "",
       age: "",
       age_pref_min: "",
       age_pref_max: "",
@@ -15,31 +16,42 @@ var editInfo = React.createClass({
 
   handleChange: function (key) {
     return function (e) {
-      var state = {};
+      let state = {};
       state[key] = e.target.value;
       this.setState(state);
     }.bind(this);
   },
 
-  handleFormSubmit: function (event) {
+  handleFormSubmit: function (e) {
 
-    event.preventDefault();
+    e.preventDefault();
 
     let userProfile = this.state;
 
     console.log(userProfile);
 
-    for (var prop in userProfile) {
+    for (let prop in userProfile) {
       if (userProfile[prop] === "") {
         userProfile[prop] = this.props[prop];
         console.log(userProfile[prop]);
       };
     };
 
-    axios.put('http://localhost:3000/api/user/update', { firstName, email, age, age_pref_min, age_pref_max }, 
-    { headers: { Authorization: cookie.load('token') } })
+    const user = cookie.load('user');
+    const emailQuery = user.email;
+    axios.put('http://localhost:3000/api/user/update', {
+      emailQuery: emailQuery,
+      firstName: userProfile.firstName,
+      is_male: userProfile.is_male,
+      seeking_male: userProfile.seeking_male,
+      age: userProfile.age,
+      age_pref_min: userProfile.age_pref_min,
+      age_pref_max: userProfile.age_pref_max
+    },
+      { headers: { Authorization: cookie.load('token') } })
       .then((response) => {
-
+        cookie.save('token', response.data.token, { path: '/' });
+        cookie.save('user', response.data.user, { path: '/' });
         window.location.href = 'http://localhost:8080/my-profile';
       })
       .catch((error) => {
@@ -52,7 +64,8 @@ var editInfo = React.createClass({
       <div className="edit-info">
         <form id="edit-info" onSubmit={this.handleFormSubmit}>
           <input onChange={this.handleChange("firstName")} name="firstName" type="text" placeholder={this.props.firstName} />
-          <input onChange={this.handleChange("email")} name="email" type="text" placeholder={this.props.email} />
+          <input onChange={this.handleChange("is_male")} name="is_male" type="text" placeholder={this.props.is_male} />
+          <input onChange={this.handleChange("is_male")} name="is_male" type="text" placeholder={this.props.seeking_male} />
           <input onChange={this.handleChange("age")} name="age" type="text" placeholder={this.props.age} />
           <input onChange={this.handleChange("age_pref_min")} name="age_pref_min" id="min" className="inline" type="text" placeholder={this.props.age_pref_min} />
           <input onChange={this.handleChange("age_pref_max")} name="age_pref_max" id="max" className="inline" type="text" placeholder={this.props.age_pref_max} />
--- client/src/components/dashboard/profile/photo-booth.js
+++ client/src/components/dashboard/profile/photo-booth.js
@@ -1,104 +1,35 @@
 const React = require('react');
 import { Link } from 'react-router';
+import { captureUserMedia, S3Upload } from './utils/rtc-utils';  
 
 const PhotoBooth = React.createClass({
-
-    // constructor(props) {
-    //     // super(props);
-    // },
-
-    render() {
-
-        var object = {
-            roomname: "jsfiddletest",
-            /*provide a valid url for signalmaster otherwise this won't work*/
-            signalmasterUrl: "example.com"
-        }
-        return (
-            <div>
-                <WebRtc obj={object} />
-            </div>
-        );
-    }
-})
-
-const WebRtc = React.createClass({
-
-    constructor(props) {
-        // super(props);
-        this.addVideo = this.addVideo.bind(this);
-        this.removeVideo = this.removeVideo.bind(this);
-        this.readyToCall = this.readyToCall.bind(this);
+    startRecord() {
+        captureUserMedia((stream) => {
+            this.state.recordVideo = RecordRTC(stream, { type: 'video' });
+            this.state.recordVideo.startRecording();
+        })
+    },
+    requestUserMedia() {
+        captureUserMedia((stream) => {
+            this.setState({ src: window.URL.createObjectURL(stream) });
+        });
     },
     componentDidMount() {
-        this.webrtc = new SimpleWebRTC({
-            localVideoEl: ReactDOM.findDOMNode(this.refs.local),
-            remoteVideosEl: "",
-            autoRequestMedia: true,
-            url: this.props.obj.signalmasterUrl
+        if (!hasGetUserMedia()) {
+            alert("Your browser cannot stream from your webcam. Please switch to Chrome or Firefox.");
+            return;
+        }
+        this.requestUserMedia();
+    },
+    captureUserMedia(callback) {
+        var params = { audio: false, video: true };
+        navigator.getUserMedia(params, callback, (error) => {
+            alert(JSON.stringify(error));
         });
-
-        console.log("webrtc component mounted");
-        this.webrtc.on('videoAdded', this.addVideo);
-        this.webrtc.on('videoRemoved', this.removeVideo);
-        this.webrtc.on('readyToCall', this.readyToCall);
     },
-
-    addVideo(video, peer) {
-        console.log('video added', peer);
-        //  console.log(this.refs.remotes);
-        var remotes = ReactDOM.findDOMNode(this.refs.remotes);
-        console.log(remotes);
-        if (remotes) {
-            var container = document.createElement('div');
-            container.className = 'videoContainer';
-            container.id = 'container_' + this.webrtc.getDomId(peer);
-            container.appendChild(video);
-            // suppress contextmenu
-            video.oncontextmenu = function () {
-                return false;
-            };
-            console.log(container);
-            remotes.appendChild(container);
-        }
-    },
-
-    removeVideo(video, peer) {
-        console.log('video removed ', peer);
-        var remotes = ReactDOM.findDOMNode(this.refs.remotes);
-        var el = document.getElementById(peer ? 'container_' + this.webrtc.getDomId(peer) : 'localScreenContainer');
-        if (remotes && el) {
-            remotes.removeChild(el);
-        }
-    },
-
-    readyToCall() {
-        return this.webrtc.joinRoom(this.props.obj.roomname);
-    },
-
-    connect() {
-        console.log("connected");
-    },
-
-    disconnect() {
-        console.log("disconnected");
-    },
-
     render() {
         return (
-            <div>
-                <video className="local"
-                    id="localVideo"
-                    ref="local" >
-                </video>
-                <div className="remotes"
-                    id="remoteVideos"
-                    ref="remotes" />
-            </div>
-        );
+            <video src={this.props.src} />
+        )
     }
 })
-
-module.exports =
-    PhotoBooth,
-    WebRtc;
