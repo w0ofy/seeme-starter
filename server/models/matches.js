218afd38d4c8bd270407b2747d2d3a6c03ee77fd
Michael <wolflearned91@gmail.com>
2017-07-01 23:37:40 -0400

    up

--- server/models/matches.js
+++ server/models/matches.js
@@ -1,14 +1,14 @@
 // Importing Node packages required for schema
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const UserSchema = require('./user');
+
 
 
 var MatchSchema = new Schema({
-    user: [{
+    user: {
         type: Schema.Types.ObjectId,
         ref: "User"
-    }]
+    }
 })
 
 
